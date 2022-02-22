d82fa7bfe045bee5b2dd020edf06de58f24224b0
RuslanPalytin <life_paradishe@mail.ru>
2022-02-22 23:06:49 +0300

    Изменения
    Изменил название переменных, убрал сортировку, сделал проверку перед записью в массив, осуществил вывод через цикл.

--- task2/src/task1.kt
+++ task2/src/task1.kt
@@ -1,28 +1,32 @@
 fun main(args: Array<String>) {
-    val MaxValue = 5
-    var ArrayName = arrayOfNulls<String>(MaxValue)
-    var ArraySurname = arrayOfNulls<String>(MaxValue)
-    var ArrayAge = arrayOfNulls<Int>(MaxValue)
-    var i = 0
+    val numberOfPeople = 3
+    var arrayOfNames = arrayOfNulls<String>(numberOfPeople)
+    var arrayOfSurnames = arrayOfNulls<String>(numberOfPeople)
+    var arrayOfAges = arrayOfNulls<Int>(numberOfPeople)
+
+    var check: Int
+    var counter = 0
 
     println("Enter the name, surname and age of the people")
-    while (i < ArrayAge.size) {
-        println("${i+1} :")
-        print("Name: ")
-        ArrayName[i] = readLine()
-        print("Surname: ")
-        ArraySurname[i] = readLine()
+    while (counter < arrayOfAges.size) {
+        println("${counter+1} :")
         print("Age: ")
-        ArrayAge[i] = readLine()!!.toInt()
-        i++
+        check = readLine()!!.toInt()
+        if (check >= 18) arrayOfAges[counter] = check
+        else{ println("The person is a minor, enter the age again!")
+            continue
+        }
+        print("Name: ")
+        arrayOfNames[counter] = readLine()
+        print("Surname: ")
+        arrayOfSurnames[counter] = readLine()
+        counter++
     }
 
-    ArrayAge.sort()
+    counter = 0
 
-    ArrayAge.forEachIndexed { index, i ->
-        if(ArrayAge[index]!! >= 18) println("${ArrayName[index]} ${ArraySurname[index]} an adult and to him ${ArrayAge[index]} age")
-        else { print("People are minors!")
-            return
-        }
+    while (counter < arrayOfAges.size) {
+        println("${arrayOfNames[counter]} ${arrayOfSurnames[counter]} an adult and to him(her) ${arrayOfAges[counter]} age")
+        counter++
     }
 }
